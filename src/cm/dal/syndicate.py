1bc052803e8afc2f91a841a4f5421dbfe122f983
Gwyn Howell <gwyn.howell@appogee.co.uk>
2013-06-11 09:33:30 +0100

    apply gpl license

--- src/cm/dal/syndicate.py
+++ src/cm/dal/syndicate.py
@@ -1,3 +1,28 @@
+# SyndicateManager
+# /src/cm/dal/syndicate.py
+#
+# Copyright (c) 2013 Gwyn Howell
+#
+# LICENSE:
+#
+# This file is part of SyndicateManager (http://my-syndicate.appspot.com).
+#
+# SyndicateManager is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+# later version.
+#
+# SyndicateManager is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with SyndicateManager.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+#
+# @author Gwyn Howell <gwyn[at]howellmail[dot]co[dot]uk>
+# @license http://www.gnu.org/licenses/gpl.html
+# @copyright 2013 Gwyn Howell
+
 from cm.models import Syndicate, UserSyndicate
 from google.appengine.ext import ndb
 
